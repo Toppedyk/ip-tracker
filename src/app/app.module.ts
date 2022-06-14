d2400f8de5a812715d507971be68e74b741da482
taylor.oppedyk <taylor.oppedyk@allata.com>
2022-06-14 15:38:43 -0600

    getting started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InfoBoardComponent } from './components/info-board/info-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoBoardComponent
   ],
   imports: [
     BrowserModule,
